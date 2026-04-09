18e841c0a456af4b456e67ed4302834a93be13c8
holden.anderson <holden.anderson@students.snow.edu>
2026-04-08 19:25:41 -0600

    i had this worrking but i think it was pure luck before

--- Countdown(Recursion_1)O54/Program.cs
+++ Countdown(Recursion_1)O54/Program.cs
@@ -1,45 +1,40 @@
 ﻿using System;
 using System.Globalization;
-//using System.Async;
 
 public class Program
 {
-	 static int UserNum;
+	public static string UserInput;
+	public static int UserNum;
 	public static void Main()
 	{
 		Console.WriteLine("Pick a number 1 through 10");
-		string userInput = Console.ReadLine();
-		//if (int.TryParse(System.Console.ReadLine(), out int UserNum));
-		{
-		}
+		//string? userInput = Console.ReadLine();
+		UserInput = Console.ReadLine();
+		//if (int.TryParse(userInput, out int UserNum));
+		int UserNum = int.Parse(UserInput);
 				//countdown T-10 seconds
-		Console.WriteLine($"T-{userInput} seconds until ignition");
-			
-		Console.WriteLine(Countdown(5)); // could i figure out how to make it run all in one go on line 22?
-		//should only write with success
-		if (UserNum == 0)
-		{
-			Console.WriteLine("Blast off!");
-		} 
+		Console.WriteLine($"T-{UserInput} seconds until ignition");
+		Console.WriteLine(Countdown(UserInput));
+
+		Console.WriteLine("Blast off!");
 	}
 
-	static int Countdown(int UserNum)
+	public static int Countdown()
 	{
-		//Console.WriteLine(UserNum);
-		if(UserNum == 0)
+		//int UserNum = int.Parse(userInput);
+		if(Usernum <= 0)
 		{
 			return 0;
 		}
 		else
 		{
-			Console.WriteLine(UserNum);
-			return UserNum * Countdown(UserNum - 1);
+			//if (int.TryParse(Program.UserInput, out int UserNum));
+
+			//the return and multiply the number by the method is not
+			// essential, this is just a use case 
+			//  return number * FactorialSolver(number - 1);
+		//whats needed here is subtraction only
+		return Countdown(UserNum - 1);
 		}
-		
-		
-		
-
 	}
-
-
-}
+}//end of whole public class program
